fed77f2cbe9687ba1a95e51ed8f7d39a0f206134
“wr11” <1127095593@qq.com>
2022-10-26 21:49:35 +0800

    plp 0.1.0 代码补充

--- server/script/containers.py
+++ server/script/containers.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import timer
+
 if "g_ListContainer" not in globals():
-    g_ListContainer = []
+	g_ListContainer = []
 
 def Regist(oContainer):
-    global g_ListContainer
-    g_ListContainer.append(oContainer)
+	global g_ListContainer
+	g_ListContainer.append(oContainer)
 
 def Init():
-    return
+	timer.Call_out(60, "listcontainer_save", SaveListContainer)
+	return
+
+def SaveListContainer():
+	return
 
 class CListContainer(list):
-    def Save(self):
-        data = []
-        for obj in self:
-            data.append(obj.Save())
-        self.clear()
-        return data
+	def Save(self):
+		data = []
+		for obj in self:
+			data.append(obj.Save())
+		self.clear()
+		return data
