89ee39d918032e1fae7f125a02102a4e0e9bd4cd
weihuawei <weihuawei@corp.netease.com>
2022-11-09 17:11:10 +0800

    plp 0.1.0 提交部分shell代码

--- ServerMgr_shell.sh
+++ ServerMgr_shell.sh
@@ -1,45 +1,68 @@
 fun_help() {
 cat <<EOF
 Usage:
-    $0 init                       # 初始化服务器配置
-    $0 start                      # 启动所有服务进程
-    $0 shutdown                   # 关闭所有服务进程
-    $0 status                     # 查看所有服务状态
-    $0 log                        # 查看所有服务日志
-    $0 kill                       # 强制杀掉所有进程
+$0 <COMMAND> [OPTION]
+
+COMMANDS:
+	init					# 初始化服务器配置
+	start					# 启动服务进程
+	shutdown				# 关闭服务进程
+	status					# 查看服务进程状态
+	log					# 查看服务日志
+	kill					# 强制杀掉服务进程
+
+OPTIONS:
+	start:
+	-A						#全部服务器
+	-H <servernum> <index>				#单个服务器
+
+	shutdown:
+	-A						#全部服务器
+	-H <servernum> <index>				#单个服务器
+
+	status:
+	-A						#全部服务器
+	-H <servernum> <index>				#单个服务器
+
+	log:
+	-[TYPE] <filename>				#查看某类型日志文件
+	TYPE: -D -E -N -W -S -> debug error notify warning stack
+
+	kill:
+	-A						#全部服务器
+	-H <servernum> <index>				#单个服务器
 EOF
 }
 
-case $# in
-    1)
-        case $1 in
-            init) fun_init ; exit $? ;;
-            start) fun_start ; exit $? ;;
-            status) fun_status ; exit $? ;;
-            log) fun_log ; exit $? ;;
-            kill) fun_kill ; exit $? ;;
-            shutdown) fun_shutdown ; exit $? ;;
+fun_init() {
+	echo "init finish"
+}
 
-            *) fun_help; exit 1 ;;
-        esac
-    ;;
-    2)
-        case $1 in
-            zdump) fun_zdump $2 ; exit $? ;;
+fun_start_all() {
+	echo "start begin"
+}
 
-            *) fun_help; exit 1 ;;
-        esac
-    ;;
-    4)
-        case $1 in
-            db) fun_db $2 $3 $4 ; exit $? ;;
+fun_start_single() {
+	serverNum=$1
+	index=$2
+	echo "${serverNum} ${index} start begin"
+}
 
-            *) fun_help; exit 1 ;;
-        esac
-    ;;
-    *)
-        case $1 in
-            *)  fun_help ; exit 1 ;;
-        esac
-    ;;
+case $1 in
+	init) fun_init ; exit $? ;;
+
+	start)
+		case $# in
+			2)
+				case $2 in
+					-A) fun_start_all ; exit $? ;;
+					*) fun_help ; exit $? ;;
+				esac
+			;;
+			3) fun_start_single $2 $3 ; exit $? ;;
+			*) fun_help ; exit 1 ;;
+		esac
+	;;
+
+	*) fun_help ; exit 1 ;;
 esac
